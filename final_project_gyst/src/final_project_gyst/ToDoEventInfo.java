d492cc80dbb29c3f707ac7c6c12bf06265e330fc
San Kim <kim503@usc.edu>
2019-04-23 17:46:13 -0700

    all calendar events work

--- final_project_gyst/src/final_project_gyst/ToDoEventInfo.java
+++ final_project_gyst/src/final_project_gyst/ToDoEventInfo.java
@@ -2,6 +2,7 @@ package final_project_gyst;
 
 class ToDoEventInfo
 {
+	public int event_ID;
 	public int user_ID;
 	public String eventname;
 	public String location;
@@ -9,8 +10,9 @@ class ToDoEventInfo
 	public String end;
 	public boolean block;
 	public String notes;
-	public ToDoEventInfo(int user_ID, String eventname, String location, String start, String end, boolean block, String notes)
+	public ToDoEventInfo(int event_ID, int user_ID, String eventname, String location, String start, String end, boolean block, String notes)
 	{
+		this.event_ID = event_ID;
 		this.user_ID = user_ID;
 		this.eventname = eventname;
 		this.location = location;
